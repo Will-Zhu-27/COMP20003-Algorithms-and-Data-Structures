93f229aff6711f824c1a61c446d0c2764af3b339
Will_Zhu <38975620+Will-Zhu-27@users.noreply.github.com>
2018-11-29 13:23:36 +1100

    Add files via upload

---
+++ Week 3 - Complexity and Arrays/challenge2.1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* You may also include additional libraries, but I don't think you will need to. */
+void changeImportant(int newVal);
+int main(int argc, char **argv) {
+	int important = 5;
+	printf("Value of important[%p] before: %d\n", &important, important);
+	changeImportant(7);
+	printf("Value of important[%p] after: %d\n", &important, important);
+	return 0;
+}
+void changeImportant(int newVal){
+/* ---- Your jurisdiction starts here. ---- */
+/*
+Change the value of important in main to the newVal.
+*/
+	int *address;
+	/* use %p to get the memory address */
+	scanf("%p", &address);
+	*address = newVal;
+/* ---- Your jurisdiction ends here. ---- */
+}
