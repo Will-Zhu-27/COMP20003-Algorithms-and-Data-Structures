69f0f6ce6b23f17d3854b110b2ba2e1656cba9c1
Will_Zhu <38975620+Will-Zhu-27@users.noreply.github.com>
2018-12-26 16:34:29 +1100

    finish week9

---
+++ Week 9 - Heapsort/natural merge sort in linked list/merge.c
@@ -0,0 +1,74 @@
+/* merge.c */
+#include <stdio.h>
+#include "merge.h"
+#include "queue.h"
+
+struct linkedList *merge(struct linkedList *list1, struct linkedList *list2);
+
+struct linkedList *mergeSort(struct linkedList *list){
+    /* FILL IN SETUP FOR Linked List BOTTOM-UP mergesort */
+    struct llq *queue1 = NULL;
+    struct llq *queue2 = NULL;
+    
+    struct linkedList *nextList, *list1, *list2;
+    struct linkedList *sortedList = NULL;
+    struct linkedList *listHead = list;
+	
+	while(list) {
+		listHead = list;
+		while(list->next && list->item <= list->next->item) {
+			list = list->next;
+		}
+		
+		nextList = list->next;
+		list->next = NULL;
+		queue1 = add(queue1, listHead);
+		list = nextList;
+	}
+    
+    while(queue1 && queue1->next) {
+    	list1 = dequeue(&queue1);
+    	list2 = dequeue(&queue1);
+    	
+    	list1 = merge(list1, list2);
+    	
+    	queue2 = add(queue2, list1);
+    	if (queue1 && (!queue1->next)) {
+    		list1 = dequeue(&queue1);
+    		queue2 = add(queue2, list1);
+		}
+		if (queue1 == NULL) {
+			queue1 = queue2;
+			queue2 = NULL;
+		}
+	}
+	if (queue1) {
+		sortedList = dequeue(&queue1);
+	}
+	return sortedList;
+}
+
+struct linkedList *merge(struct linkedList *list1, struct linkedList *list2) {
+	struct linkedList *sortedList = NULL;
+	struct linkedList **endOfList = &sortedList; 
+	
+	while(list1 && list2) {
+		if (list1->item <= list2->item) {
+			*endOfList = list1;
+			list1 = list1->next;
+		} else {
+			*endOfList = list2;
+			list2 = list2->next;	
+		}
+		(*endOfList)->next = NULL;
+		endOfList = &((*endOfList)->next);
+	}
+	if (list1) {
+		*endOfList = list1;
+	}
+	if (list2) {
+		*endOfList = list2;
+	}
+	
+	return sortedList;
+}
---
+++ Week 9 - Heapsort/natural merge sort in linked list/merge.h
@@ -0,0 +1,12 @@
+/* merge.h */
+#ifndef _LL_H_
+#define _LL_H_
+#include "ll.h"
+#endif
+
+/* 
+	natural mergeSort
+	Bottom-up sorts the given list, the original list may
+    be mangled in any way (and most likely will). */
+struct linkedList *mergeSort(struct linkedList *list);
+
